26452ab471bb21c551470c0362c115bc1bbf2efe
kimberly lin <kimberly.lin10@gmail.com>
2021-09-27 17:38:18 -0700

    finished cards and had code review

--- number facts/deck-of-cards/cards.js
+++ number facts/deck-of-cards/cards.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const API_BASE_URL = 'http://deckofcardsapi.com/api/deck/';
+const $cardPile = $(".card-pile")
 
 let deck;
 let deckId;
@@ -11,21 +12,30 @@ async function shuffleDeck() {
 }
 
 async function drawCard() {
-    const resp = await axios.get(`${API_BASE_URL}${deckId}/draw/?count=1`)
+    const resp = await axios.get(`${API_BASE_URL}${deckId}/draw/?count=1`);
     return resp.data.cards[0];
 }
 
 async function drawAndShowCards() {
-    const card1 = await drawCard()
-    const card2 = await drawCard()
+    const card1 = await drawCard();
+    const card2 = await drawCard();
 
-    console.log("Card1:",  card1.value,  card1.suit);
-    console.log("Card2:",  card2.value,  card2.suit);
+    console.log("Card1:", card1.value, card1.suit);
+    console.log("Card2:", card2.value, card2.suit);
 }
-// Why does await not work inside of console.log?
 
 async function start() {
     deck = await shuffleDeck();
     deckId = deck.deck_id;
 }
 
+async function drawAndShowCard() {
+    const card = await drawCard();
+
+    const imgHTML = `<img src= ${card.image}></img>`;
+
+    $cardPile.append(imgHTML);
+}
+
+start();
+$("button").on("click", drawAndShowCard);
--- number facts/num facts.js
+++ number facts/num facts.js
@@ -12,12 +12,12 @@ async function getBatchFacts(num1, num2, num3, num4) {
     return resp.data;
 }
 
-function getFourFacts(num) {
+async function getFourFacts(num) {
     const resp1 = getFact(num);
     const resp2 = getFact(num);
     const resp3 = getFact(num);
     const resp4 = getFact(num);
-    return Promise.all([resp1, resp2, resp3, resp4]);
+    return await Promise.all([resp1, resp2, resp3, resp4]);
 }
 // Question: Why don't we need to have await in front of Promise.all(), shouldn't that always return a promise?
 // getFourFacts works without an async in front of function definition. 
